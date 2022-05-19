b2baf254e5c1bc7c287b995296dfa2af85b3588a
AmericoG <gabriel.americodc@outlook.com>
2022-05-19 20:33:44 -0300

    navegação entre os métodos

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,44 @@
+import services.FuncionarioService;
+
+import java.util.Scanner;
+
 public class Programa {
 
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        FuncionarioService funcionarioService = new FuncionarioService();
+
+        boolean execucao = true;
+
+        while (execucao) {
+            System.out.println("=== Manter funcionário ===");
+            System.out.println();
+            System.out.println("1. Salvar um novo funcionário");
+            System.out.println("2. Atualizar dados de funcionário");
+            System.out.println("3. Listar funcionários");
+            System.out.println("4. Remover um funcionário");
+
+            String opcao = sc.nextLine();
+
+            switch (opcao) {
+                case "1":
+                    funcionarioService.save();
+                    break;
+                case "2":
+                    funcionarioService.update();
+                    break;
+                case "3":
+                    funcionarioService.getAll();
+                    break;
+                case "4":
+                    funcionarioService.delete();
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        }
+    }
 }
--- src/services/FuncionarioService.java
+++ src/services/FuncionarioService.java
@@ -3,19 +3,19 @@ package services;
 public class FuncionarioService {
 
     public void save() {
-
+        System.out.println("Método save");
     }
 
     public void update() {
-
+        System.out.println("Método update");
     }
 
     public void getAll() {
-
+        System.out.println("Método getAll");
     }
 
     public void delete() {
-
+        System.out.println("Método delete");
     }
 
 
